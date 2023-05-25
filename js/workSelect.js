3aec420482f39e2d63fc0f25acdc5e6606602327
richDad <richdad6208@gmail.com>
2023-05-25 17:19:35 +0900

    first

--- js/workSelect.js
+++ js/workSelect.js
@@ -1,15 +1,16 @@
-const figure = document.querySelectorAll(".work_wrap figcaption");
-const closeButton = document.querySelectorAll(".work_wrap figcaption button");
+const img = document.querySelectorAll(".work_wrap > figure > img");
+const figcaption = document.querySelectorAll(".work_wrap figcaption")
+const button = document.querySelectorAll(".figcaption_wrap > button");
 
-closeButton.forEach((item, index) => {item.addEventListener("click", () => {
-figure[index].classList.remove("workSelect");
-console.log(figure[index].className);
-}
-)})
-
-figure.forEach((item,index) => item.addEventListener("click", () => {
-for(i=0; i<figure.length; i++) {
-figure[i].classList.remove("workSelect");
-}
-figure[index].classList.toggle("workSelect");
+button.forEach((item, index) => item.addEventListener("click", () => {
+figcaption[index].classList.remove("workSelect");
 }))
+
+img.forEach((item, index) =>item.addEventListener("click", ()=> {
+for (i=0; i<img.length; i++) {
+figcaption[i].classList.remove("workSelect");
+}
+figcaption[index].classList.add("workSelect");
+}
+)
+);
